549b4a16a9d15b91cfd66a7f47b3bfd455db9591
Vasya Turok <turokvasil0@gmail.com>
2023-10-27 00:45:30 +0300

    fix static

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+from .models import DishTypes, Dish, Ingredient, Cook
+
+
+def index(request):
+    """View function for the home page of the site."""
+
+    num_cooks = Cook.objects.count()
+    num_dishes = Dish.objects.count()
+
+    context = {
+        "num_drivers": num_cooks,
+        "num_cars": num_dishes,
+    }
+
+    return render(request, "restaurant/index.html", context=context)
--- restaurant_kitchen_service/urls.py
+++ restaurant_kitchen_service/urls.py
@@ -21,5 +21,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
+    path("", include("restaurant.urls", namespace="restaurant")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
