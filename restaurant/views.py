e791dd4d2752c5148121f706780867c7b48e4684
Vasya Turok <turokvasil0@gmail.com>
2023-10-28 00:54:17 +0300

    add dish_list & paginations & header & forms.py & search form

--- restaurant/views.py
+++ restaurant/views.py
@@ -5,6 +5,7 @@ from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
 
+from .forms import SearchForm
 from .models import DishTypes, Dish, Ingredient, Cook
 
 
@@ -20,3 +21,25 @@ def index(request):
     }
 
     return render(request, "restaurant/index.html", context=context)
+
+
+class DishListView(LoginRequiredMixin, generic.ListView):
+    model = Dish
+    context_object_name = "dish_list"
+    template_name = "restaurant/dish_list.html"
+    paginate_by = 20
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(DishListView, self).get_context_data(**kwargs)
+        search = self.request.GET.get("search", "")
+        context["search_form"] = SearchForm(initial={"search": search})
+        return context
+
+    def get_queryset(self):
+        queryset = Dish.objects.all()
+        form = SearchForm(self.request.GET)
+        if form.is_valid():
+            return queryset.filter(
+                name__icontains=form.cleaned_data["search"]
+            )
+        return queryset
