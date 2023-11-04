48605396c49da78edfeb65ff5bbe2d117f417f21
Vasya Turok <turokvasil0@gmail.com>
2023-11-04 18:25:22 +0200

    add index content and styles

--- restaurant/views.py
+++ restaurant/views.py
@@ -15,10 +15,12 @@ def index(request):
 
     num_cooks = Cook.objects.count()
     num_dishes = Dish.objects.count()
+    num_dish_types = DishTypes.objects.count()
 
     context = {
-        "num_drivers": num_cooks,
-        "num_cars": num_dishes,
+        "num_cooks": num_cooks,
+        "num_dishes": num_dishes,
+        "num_dish_types": num_dish_types,
     }
 
     return render(request, "restaurant/index.html", context=context)
