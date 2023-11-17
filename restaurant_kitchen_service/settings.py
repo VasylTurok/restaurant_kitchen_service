c5bd3f6ab3b4ffa8d66a5c04f44b2ac5b63c9fce
Vasyl Turok <turokvasil0@gmail.com>
2023-11-17 22:24:26 +0200

    add element to ALLOWED_HOSTS

--- restaurant_kitchen_service/settings.py
+++ restaurant_kitchen_service/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 # DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "restaurant-kitchen-service-vpi6.onrender.com"]
 
 
 # Application definition
